a60aea7ef142bba2a13e6ff60f6c9dee100bdbfc
ElvisMaina <maish254.01@gmail.com>
2022-02-16 12:38:55 +0300

    second commit

--- Initialize.cpp
+++ Initialize.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-	return 0;
+int num1;
+int num2;
+int fn;
+	
+cout<<"Enter first number : "<<endl;
+cin>>num1;
+cout<<"Enter second number : "<<endl;
+cin>>num2;
+
+fn = num1 > num2 num1:num2 ; 
+	return 0; 
 }
